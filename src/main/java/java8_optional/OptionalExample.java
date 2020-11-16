7e2933283c30336fec300b2967f24230d98c698f
Ladislav <ladislav.szabo01@t-systems.com>
2020-11-16 12:50:17 +0100

    Adding some additional code

--- src/main/java/java8_optional/OptionalExample.java
+++ src/main/java/java8_optional/OptionalExample.java
@@ -8,9 +8,46 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 
+ class Bar {
 
+    private String foo;
+
+    public Optional<String> getFoo() {
+
+        return Optional.ofNullable(foo);
+
+    }
+
+}
 
 public class OptionalExample {
+    /*
+    !!!
+        A variable whose type is Optional should never itself be null; it should always point to an Optional instance.
+        the method should return an empty Optional instead of null in all cases
+
+        In general, an Optional should be used as a return value if:
+
+            It is expected that a value may or may not be present
+            It is not an error if a value is missing
+            The client is responsible for handling the case of a missing value
+
+        Of course, people will do what they want.
+        But we did have a clear intention when adding this feature,
+        and it was not to be a general purpose Maybe type, as much as many people
+        would have liked us to do so. Our intention was to provide a limited mechanism for
+        library method return types where there needed to be a clear way to represent "no result", and using
+        null for such was overwhelmingly likely to cause errors. - see up example
+
+        Optional do not support serializable interface, therefore do not used it as instance variable
+
+        Do not used optional parameter as argument, instead used method overloading - two method one got and one not got argument
+        or use another method - one which requeire argument, other which not require argument
+
+        you can also introduced the null object - https://refactoring.guru/introduce-null-object
+
+    !!!
+     */
 
     // == START creation of optional objects ==
     @Test
@@ -38,6 +75,9 @@ public class OptionalExample {
         Optional<String> opt = Optional.ofNullable(name);
         Assert.assertFalse(opt.isPresent());
     }
+
+    // == STOP creation of optional objects ==
+
 
     // == Conditional ifPresent() ==
     @Test
@@ -83,6 +123,9 @@ public class OptionalExample {
     @Test
     public void givenOptional_whenGetsValue_thenCorrect() {
         Optional<String> opt = Optional.of("Laci");
+//        ...One of the few mistakes we made [in Java 8] was the naming of Optional.get(),
+//        because the name just invites people to call it without calling isPresent(),
+//        undermining the whole point of using Optional in the first place....
         String name = opt.get();
         Assert.assertEquals("Laci", name);
     }
